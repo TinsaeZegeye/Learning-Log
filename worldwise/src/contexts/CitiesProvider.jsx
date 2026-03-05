dc01ded0247e2eaeedaeebd8271b04ab96747fe7
tinsae zegeye <tinsaezegeye@gmail.com>
2026-03-05 14:45:59 +0300

    Finilazie worldwise application by adding some performance optimazation techniques

--- worldwise/src/contexts/CitiesProvider.jsx
+++ worldwise/src/contexts/CitiesProvider.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 import { CitiesContext } from "./CitiesContext";
 
 const BASE_URL = "http://localhost:9000";
@@ -61,21 +61,24 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (currentCity.id === Number(id)) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (currentCity.id === Number(id)) return;
 
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "Something went wrong while getting the City",
-      });
-    }
-  }
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "Something went wrong while getting the City",
+        });
+      }
+    },
+    [currentCity.id],
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
