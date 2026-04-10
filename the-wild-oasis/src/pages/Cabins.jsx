d08357798bff77a0e2960b8a6fb331543be05d67
tinsae zegeye <tinsaezegeye@gmail.com>
2026-04-10 11:03:53 +0300

    Setup supabase and create required tables and storage buckets

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
+
+import { getCabins } from "../services/apiCabins";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+
+      <img
+        src="https://svsoiaezspzbpoznepnc.supabase.co/storage/v1/object/public/cabins-image/cabin-001.jpg"
+        alt="Cabin 001"
+      />
     </Row>
   );
 }
